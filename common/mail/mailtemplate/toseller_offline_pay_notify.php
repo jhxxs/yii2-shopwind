0ed1aad3a30b3bc4573a9f8bfa1119f442548123
shopwind <shopwind@qq.com>
2021-11-19 17:09:55 +0800

    修复邮件通知问题

--- common/mail/mailtemplate/toseller_offline_pay_notify.php
+++ common/mail/mailtemplate/toseller_offline_pay_notify.php
@@ -8,7 +8,7 @@ return array (
 <p style="padding-left: 30px;">查看订单详细信息请点击以下链接</p>
 <p style="padding-left: 30px;"><a href="{url route=\'seller_order/view\' order_id=$order.order_id}">{url route=\'seller_order/view\' order_id=$order.order_id}</a></p>
 <p style="padding-left: 30px;">查看您的订单列表管理页请点击以下链接</p>
-<p style="padding-left: 30px;"><a href="{url route=\'seller_order/index\'}</a></p>
+<p style="padding-left: 30px;"><a href="{url route=\'seller_order/index\'}">{url route=\'seller_order/index\'}</a></p>
 <p style="text-align: right;">{$site_name}</p>
 <p style="text-align: right;">{$send_time}</p>',
 );
