28996809a8c94e0f8d73b650a53e1901d05e1f47
shopwind <shopwind@qq.com>
2022-09-23 15:43:13 +0800

    修复已知问题

--- frontend/controllers/WeixinController.php
+++ frontend/controllers/WeixinController.php
@@ -12,6 +12,7 @@
 namespace frontend\controllers;
 
 use Yii;
+use yii\helpers\Url;
 use yii\helpers\ArrayHelper;
 
 use common\models\UserModel;
@@ -43,122 +44,111 @@ class WeixinController extends \common\controllers\BaseMallController
 		$this->params = ArrayHelper::merge($this->params, Page::getAssign('mall'));
 	}
 
-    public function actionIndex()
-    {
+	public function actionIndex()
+	{
 		$wx = new Weixin();
-		
+
 		// 验证
-		if(!$wx->valid()) {
+		if (!$wx->valid()) {
 			return Message::warning($wx->errors);
 		}
-		
+
 		$postData = $wx->getPostData();
-		if($postData['MsgType'] == 'event') //接收事件推送
+		if ($postData['MsgType'] == 'event') //接收事件推送
 		{
-			if($postData['Event'] == 'subscribe') //关注事件
+			if ($postData['Event'] == 'subscribe') //关注事件
 			{
-				if($wx->config['autologin']) {
+				if ($wx->config['autologin']) {
 					$this->register($postData['FromUserName']);
 				}
-					
-				if(($reply = WeixinReplyModel::find()->where(['userid' => 0, 'action' => 'beadded'])->asArray()->one()))
-				{
-					if($reply['type']) $content[] = $reply;
-					else $content = $reply['content'];
-				}
-				else
-				{
+
+				if (($reply = WeixinReplyModel::find()->where(['userid' => 0, 'action' => 'beadded'])->asArray()->one())) {
+					if ($reply['type']) $content[] = $reply;
+					else $content = $reply['description'];
+				} else {
 					$wxInfo = $wx->getUserInfo($postData['FromUserName']);
-					$content = $wxInfo->nickname .' 您好，欢迎您关注' . $wx->config['name'] .'! <a href="' . Url::toRoute(['weixin/signin', 'openid' => $postData['FromUserName']]).'">【会员中心】</a>';
+					$content = $wxInfo->nickname . ' 您好，欢迎您关注' . $wx->config['name'] . '! <a href="' . Url::toRoute(['weixin/signin', 'openid' => $postData['FromUserName']]) . '">【会员中心】</a>';
 				}
-			}
-			elseif($postData['Event'] == 'CLICK') //点击菜单拉取消息时的事件推送,后台设定为图文消息
+			} elseif ($postData['Event'] == 'CLICK') //点击菜单拉取消息时的事件推送,后台设定为图文消息
 			{
-				if(!($reply = WeixinReplyModel::find()->where(['reply_id' => intval($postData['EventKey'])])->asArray()->one())) {
+				if (!($reply = WeixinReplyModel::find()->where(['reply_id' => intval($postData['EventKey'])])->asArray()->one())) {
 					return false;
 				}
 				$content[] = $reply;
 			}
-		}
-		else
-		{
+		} else {
 			//先执行回复命令，再找关键字，再自动回复
 			$getContent = $postData['Content'];
 
 			//关键词命令
-			if($getContent) $reply = $this->checkKeywords($getContent);
+			if ($getContent) $reply = $this->checkKeywords($getContent);
 
 			//关键字回复
-			if($reply)
-			{
+			if ($reply) {
 				//图文消息
 				$content = $reply;
-			}
-			else
-			{
+			} else {
 				//自动回复
-				if(($autoreply = WeixinReplyModel::find()->where(['userid' => 0, 'action' => 'autoreply'])->asArray()->one()))
-				{
+				if (($autoreply = WeixinReplyModel::find()->where(['userid' => 0, 'action' => 'autoreply'])->asArray()->one())) {
 					//图文消息
-					if($autoreply['type']) $content = $autoreply;
+					if ($autoreply['type']) $content = $autoreply;
 					else $content = $autoreply['description'];
 				}
 			}
 		}
 
-		if(!($resultStr = $wx->getMsgXML($postData['FromUserName'], $postData['ToUserName'], $content))) {
+		if (!($resultStr = $wx->getMsgXML($postData['FromUserName'], $postData['ToUserName'], $content))) {
 			return false;
 		}
-		
+
 		exit($resultStr);
-    }
-	
+	}
+
 	/* 自动注册并登陆 */
 	private function register($openid = '')
 	{
-		if(!$openid) {
+		if (!$openid) {
 			return false;
 		}
-		
+
 		$wx = new Weixin();
-		
-		if(!($wxInfo = $wx->getUserInfo($openid))) {
+
+		if (!($wxInfo = $wx->getUserInfo($openid))) {
 			return false;
 		}
 		$query = BindModel::find()->where(['and', ['or', 'openid' => $openid, 'unionid' => $openid], ['app' => 'weixin']])->one();
-		if($query && UserModel::find()->where(['userid' => $query->userid])->exists()) {
+		if ($query && UserModel::find()->where(['userid' => $query->userid])->exists()) {
 			return false;
 		}
-		
+
 		$bind = (object)array('code' => 'weixin', 'unionid' => $openid, 'portrait' => $wxInfo->headimgurl, 'nickname' => $wxInfo->nickname, 'access_token' => $wxInfo->access_token);
-		
+
 		do {
 			$model = new \frontend\models\UserRegisterForm();
-			if($bind->nickname) {
+			if ($bind->nickname) {
 				$model->username = Basewind::checkUser($bind->nickname) ? $bind->nickname : UserModel::generateName($bind->nickname);
 			} else $model->username = UserModel::generateName($bind->code);
 			$model->password  = mt_rand(1000, 9999);
 			$user = $model->register(['real_name' => $bind->nickname, 'portrait' => $bind->portrait]);
 		} while (!$user);
-		
+
 		// 将绑定信息插入数据库
-		if(!BindModel::bindUser($bind, $user->userid)) {
+		if (!BindModel::bindUser($bind, $user->userid)) {
 			return false;
 		}
-		
+
 		return $user->userid;
 	}
-	
+
 	private function checkKeywords($word = '')
 	{
 		$result = [];
 		$replys = WeixinReplyModel::find()->where(['and', ['userid' => 0, 'action' => 'smartreply'], ['like', 'keywords', $word]])->asArray()->all();
 
-		foreach($replys as $key => $val)
-		{
-			$keywords = explode(',',str_replace('，', ',', trim($val['keywords'])));
+		foreach ($replys as $key => $val) {
+			$keywords = explode(',', str_replace('，', ',', trim($val['keywords'])));
 
-			if(in_array($word, $keywords)) {
+			if (in_array($word, $keywords)) {
 				$result[] = $val;
 			}
 		}
