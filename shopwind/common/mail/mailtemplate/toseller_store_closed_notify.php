ea23d89f5300fafe23596c766643fc6fe8f25c23
shopwind@qq.com <shopwind@qq.com>
2023-10-26 15:13:03 +0800

    更新短消息模版

--- shopwind/common/mail/mailtemplate/toseller_store_closed_notify.php
+++ shopwind/common/mail/mailtemplate/toseller_store_closed_notify.php
@@ -1,9 +1,9 @@
 <?php
 return array (
   'version' => '1.0',
-  'subject' => '{$site_name}提醒:您的店铺【{$store.store_name}】已被关闭',
-  'content' => '<p>尊敬的{$store.owner_name}:</p>
-<p style="padding-left: 30px;">您的店铺【{$store.store_name}】因为【{$reason}】被平台关闭，如有疑问，请联系客服。</p>
-<p style="text-align: right;">{$site_name}</p>
-<p style="text-align: right;">{$send_time}</p>',
+  'subject' => '{$site_name}提醒：您的店铺【{$store.store_name}】已被关闭',
+  'content' => '<p class="mb10 bold">尊敬的{$store.owner_name}:</p>
+<p>您的店铺【{$store.store_name}】因为【{$reason}】被平台关闭，如有疑问，请联系客服。</p>
+<p class="f-gray f-12 bt pt10 mt20">{$site_name}</p>
+<p class="f-gray f-12">{$send_time}</p>',
 );
