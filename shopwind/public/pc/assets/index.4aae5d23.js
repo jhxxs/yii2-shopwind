1751d45d62595c11396735984f7d9bdb9ada6257
shopwind@qq.com <shopwind@qq.com>
2023-11-12 14:13:11 +0800

    更新编译包

---
+++ shopwind/public/pc/assets/index.4aae5d23.js
@@ -0,0 +1,1 @@
+import{_ as a}from"./blocks.977b042b.js";import{_ as r}from"./home.e5e73006.js";import{_ as o}from"./home.15779fce.js";import{o as s,c as t,a as e,F as m}from"./index.69005660.js";import"./chat.3bde36fb.js";const u={setup(_){return(n,c)=>(s(),t(m,null,[e(r,{page:"index"}),e(a,{page:"index",header:!1,footer:!1}),e(o,{page:"index"})],64))}};export{u as default};
---
+++ shopwind/public/pc/assets/page.31165c0e.js
@@ -0,0 +1,1 @@
+import{u as r,j as a,o,g as s,k as t}from"./index.69005660.js";import{_ as c}from"./blocks.977b042b.js";const f={setup(p){const e=r();return(u,m)=>a(e).params?(o(),s(c,{key:0,page:a(e).params.id},null,8,["page"])):t("",!0)}};export{f as default};
