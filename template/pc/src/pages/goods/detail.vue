27aa5b16c87f51c2f60aa418831e6ef01c6068e6
Kyle <iamjhxxs@gmail.com>
2025-05-12 16:49:14 +0800

    feat: 移除商品详情页 banner

--- template/pc/src/pages/goods/detail.vue
+++ template/pc/src/pages/goods/detail.vue
@@ -1,7 +1,6 @@
 <template>
 	<myhead :exclude="['category', 'imagead']"></myhead>
 	<div class="main bgf">
-		<div class="banner" :style="{ 'background-image': store.pcbanner ? 'url(' + store.pcbanner + ')' : '' }"></div>
 		<div class="menus">
 			<ul class="uni-flex uni-row w f-white">
 				<li @click="redirect('/store/index/' + goods.store_id)" class="pointer pl20 pr20"><span
